719c003e4afa3c446018d6d225a4a6ab56f47d67
Sameer Kulkarni <sameer@buybak.io>
2025-05-23 13:53:00 -0500

    #BBK-012 UI UX changes

--- react_frontend/src/views/components/screens/MobileMain/MobileMessage.jsx
+++ react_frontend/src/views/components/screens/MobileMain/MobileMessage.jsx
@@ -53,7 +53,7 @@ export const  MobileMessage = (props) => {
     const fbgc = (props.estimuli === "ForecastEvent") ? "#CCF" : nbgc;
     return (
         <div className="mx-4">
-            <Grid container spacing={2} padding={2} >
+            <Grid container spacing={1} padding={1} >
             {
                 // If the user who sent the message is the currentUser
                 props.user === currentUser ? (
@@ -62,28 +62,22 @@ export const  MobileMessage = (props) => {
                     <Grid item xs="9">
                     <ColorSubCard
                       padding={1}
-                      spacing={1}
+                      spacing={0}
                       border={'red'}
                       background={'blue'}
                       align-items="right"
                       md={8}
                       aria-label="main mailbox folders"
-                      sx={{ boxShadow: '0px 0px 0px #000', border: '3px solid', borderRadius: '30px', background: "lightblue" }}
+                      sx={{ boxShadow: '0px 0px 0px #000', border: '2px solid', borderRadius: '30px', background: "lightblue" }}
                     >
-                        <div className="flex items-end justify-end">
-                            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order- items-end">
-                                <div>
                                     <span 
-                                        className="px-4 py-2 rounded-lg inline-block max-w-sm break-all float-right rounded-br-none bg-blue-600 text-white "
+                                        className="px-2 py-2 rounded-lg inline-block max-w-sm break-all float-right rounded-br-none bg-blue-600 text-white "
                                         style={{color: 'black'}}
                                         dangerouslySetInnerHTML={{ __html: props.msg}}
                                         > 
                                     </span> <br/>
                                         {/* props.user || "Guest User" } - {formatDate(new Date())*/}
                                     <Typography style={{ color: "gray", fontFamily: 'tiempos-headline,Lucida,Georgia,serif', fontWeight: 'normal', fontSize: "0.9rem" }}><small style={{color: 'blue'}}>{ props.user || "Guest User"  }</small>&nbsp;{props.etype}{': '}&nbsp;{props.estate}{'( '}{props.estimuli}&nbsp;{')'}</Typography>
-                                </div>
-                            </div>
-                        </div>
                     </ColorSubCard>
                     </Grid>
                     </>
@@ -92,17 +86,14 @@ export const  MobileMessage = (props) => {
                     <Grid item xs="9">
                         <ColorSubCard
                           padding={1}
-                          spacing={1}
+                          spacing={0}
                           border={'red'}
                           background={'blue'}
                           align-items="left"
                           md={8}
                           aria-label="main mailbox folders"
-                          sx={{ boxShadow: '0px 0px 0px #000', border: '3px solid', borderRadius: '30px', background: fbgc}}
+                          sx={{ boxShadow: '0px 0px 0px #000', border: '1px solid', borderRadius: '30px', background: fbgc}}
                         >
-                        <div className="flex items-end">
-                            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-                                <div>
                                     <span 
                                         className="px-4 py-2 rounded-lg inline-block max-w-sm break-all rounded-bl-none bg-gray-800 text-gray-100"
                                         style={{color: 'black', fontSize: '1.1rem'}}
@@ -113,9 +104,6 @@ export const  MobileMessage = (props) => {
                                     <small style={{color: 'gray'}}>&nbsp;{props.etype}{': '}</small>
                                     <small style={{color: 'black'}}>&nbsp;{props.estate}</small>
                                     <small style={{color: 'red'}}>{'( '}{props.estimuli}&nbsp;{')'}</small>
-                                </div>
-                            </div>
-                        </div>
                     </ColorSubCard>
                     </Grid>
                     <Grid item xs="3" />
